1b85ef97c236eccccac16e9a678dc78d1b7547e2
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-09-12 20:38:18 -0700

    Format heading topography test

--- tests/test_heading_topography_review.py
+++ tests/test_heading_topography_review.py
@@ -15,12 +15,30 @@ def test_heading_topography_review_tracks_pb_alignment() -> None:
                 "source": "EPG",
                 "target": "PFL3",
                 "edges": [
-                    {"source": 1, "target": 10, "source_instance": "EPG(PB08)_R4", "target_instance": "PFL3(PB12c)_R4_C4", "weight": 12.0},
-                    {"source": 2, "target": 11, "source_instance": "EPG(PB08)_L3", "target_instance": "PFL3(PB12c)_L3_C5", "weight": 7.0},
-                    {"source": 3, "target": 12, "source_instance": "EPG(PB08)_R4", "target_instance": "PFL3(PB12c)_R3_C5", "weight": 14.0}
-                ]
+                    {
+                        "source": 1,
+                        "target": 10,
+                        "source_instance": "EPG(PB08)_R4",
+                        "target_instance": "PFL3(PB12c)_R4_C4",
+                        "weight": 12.0,
+                    },
+                    {
+                        "source": 2,
+                        "target": 11,
+                        "source_instance": "EPG(PB08)_L3",
+                        "target_instance": "PFL3(PB12c)_L3_C5",
+                        "weight": 7.0,
+                    },
+                    {
+                        "source": 3,
+                        "target": 12,
+                        "source_instance": "EPG(PB08)_R4",
+                        "target_instance": "PFL3(PB12c)_R3_C5",
+                        "weight": 14.0,
+                    },
+                ],
             }
-        ]
+        ],
     }
     report = review_heading_topography(audit)
     threshold_10 = report["threshold_reports"]["10"]
