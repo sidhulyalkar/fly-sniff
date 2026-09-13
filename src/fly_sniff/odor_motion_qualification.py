aab58d76d7def7a83171e75293ea7b0e8313e4fa
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-09-12 23:57:43 -0700

    Wire odor-motion qualification imports

--- src/fly_sniff/odor_motion_qualification.py
+++ src/fly_sniff/odor_motion_qualification.py
@@ -1,3 +1,7 @@
 from __future__ import annotations
 
-# Controller-free prefunctional qualification for bilateral odor timing.
+from .config import ArenaConfig, PlumeConfig, SensorConfig
+from .odor_motion import load_odor_motion_config
+from .odor_motion_edge_assay import run_edge_assay
+from .odor_motion_plume_assay import run_plume_assay
+from .odor_motion_qualification_receipt import qualification_sha256
